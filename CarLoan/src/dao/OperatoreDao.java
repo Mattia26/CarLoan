13e0fa4d9649716c09e0047e0e822848b7700a6d
Mattia <mattia0260@gmail.com>
2015-07-12 16:45:41 +0200

    ok

--- CarLoan/src/dao/OperatoreDao.java
+++ CarLoan/src/dao/OperatoreDao.java
@@ -5,9 +5,9 @@ import entity.Operatore;
 
 public interface OperatoreDao {
 	public boolean inserisciOperatore(String nome, String cognome, String indirizzo, 
-			int numTelefono, String nickname);
+			String numTelefono, String nickname);
 	public boolean modificaDatiOperatore(String nome, String cognome, String indirizzo, 
-			int numTelefono, String nickname);
+			String numTelefono, String nickname);
 	public boolean rimuoviOperatore(String nickname);
 	public ArrayList<Operatore> getOperatori();
 }
--- CarLoan/src/presentation/ApplicationController.java
+++ CarLoan/src/presentation/ApplicationController.java
@@ -11,6 +11,7 @@ import presentation.command.GetDatiCliente;
 import presentation.command.GetDatiContratto;
 import presentation.command.Initialize;
 import presentation.command.InserimentoManutenzione;
+import presentation.command.InserimentoOperatore;
 import presentation.command.ModificaContratto;
 import presentation.command.ModificaDatiCliente;
 import presentation.command.ModificaDatiOperatore;
@@ -92,6 +93,16 @@ public class ApplicationController implements ApplicationControllerI {
 		case "Initialize":
 			command = new Initialize();
 			ritorno = command.Execute("");
+		break;
+		
+		case "NuovoOperatore":
+						dispatcher = new ViewDispatcher();
+						dispatcher.setInterface("NuovoOperatore.fxml");
+		break;
+					
+		case "MenuAmministratore":
+					dispatcher = new ViewDispatcher();
+					dispatcher.setInterface("menuAmministratore.fxml");
 		break;
 		}
 		
@@ -184,7 +195,12 @@ public class ApplicationController implements ApplicationControllerI {
 		case "InserisciManutenzione":
 			command = new InserimentoManutenzione();
 			ritorno = command.Execute(parameters.get(0));
-		
+		break;
+			
+		case "NuovoOperatore":
+			command = new InserimentoOperatore();
+			ritorno = command.Execute(parameters);
+		break;
 		}
 		return ritorno;
 	}
@@ -193,7 +209,7 @@ public class ApplicationController implements ApplicationControllerI {
 		GestioneSessione.setNomeOperatore("Mario");
 		GestioneSessione.setCognomeOperatore("Rossi");
 		GestioneSessione.setTelefonoOperatore("3333333333");
-		return "operatore";
+		return "amministratore";
 	}
 
 }
