4092e4415bf9c30eb39ce576a39647c5055d4385
Mattia <mattia0260@gmail.com>
2015-07-14 13:13:58 +0200

    javadoc utility

--- CarLoan/src/utility/LoginUtility.java
+++ CarLoan/src/utility/LoginUtility.java
@@ -10,39 +10,57 @@ import java.util.HashMap;
 
 public class LoginUtility implements Serializable{
 
-	static HashMap<String, String> i=getCurrentUserList(); 
+	/**
+	 * Map contenente  username, password e indirizzi email degli utenti registrati
+	 */
+	static HashMap<String, String> i=getCurrentUserList();
 	
+	/**
+	 * Aggiunge un utente indicandone username e password, quindi salva la Map nel file
+	 * @param username
+	 * @param password
+	 */
 	public void insertUser(String username, String password) { 
 	i.put(username, cryptPassword(password));
 	salva();
 	}
-
+	
+	/**
+	 * Verifica se l'username in input è già contenuto nella Map
+	 * @param username Username da verificare
+	 * @return Booleano esito della verifica
+	 */
 	public boolean containsUser(String username) { 
 		return i.containsKey(username);
 	}
 	
-	public boolean deleteUser(String username) {
-		try {
-			if(i.remove(username).equals(""))
-				return false;
-			else 
-				return true;
-		}
-		catch (NullPointerException e) {
-			return false;
-		}
-	}
-
+	/**
+	 * Restituisce un vettore contenete la password dell'username in input
+	 * @param username Username da cercare
+	 * @return Password del dato username
+	 */
 	private String getPassword(String username) { 
 		return i.get(username); 
 	}
-
+	
+	/**
+	 * Verifica la correttezza della password in input per l'username in input
+	 * @param username 
+	 * @param password
+	 * @return Booleano risultato della verifica
+	 */
 	public boolean correctPassword(String username, String password) { 
+		// verifica la correttezza della password ricevuta, per l'username in input
 		if(this.containsUser(username))   
 			return (cryptPassword(password).equals(this.getPassword(username)));
 		else return false;
 	}
-
+	
+	/**
+	 * Funzione di criptaggio delle password
+	 * @param password
+	 * @return Stringa rappresentante la password criptata
+	 */
 	private String cryptPassword(String password)
 	   {
 	   try {
@@ -61,17 +79,25 @@ public class LoginUtility implements Serializable{
 			return "password";
 		}
 	   }
-
+	
+	/**
+	 * Metodo per il caricamento della lista degli utenti dal file
+	 * @return HashMap contenente la lista degli utenti con relative password
+	 */
 	private static HashMap<String, String> getCurrentUserList() {
 		try {
 			return LoginFileUtility.carica();
 		}
-		catch(ClassNotFoundException | IOException e) { 
+		catch(ClassNotFoundException | IOException e) { // altrimenti crea una nuova map
 			return new HashMap<String, String>();
 		}
 	}
-
-	private boolean salva() {
+	
+	/**
+	 * Metodo per il salvataggio su file della lista utenti con relative password
+	 * @return  Esito del salvataggio
+	 */
+	private boolean salva() { 
 		try {
 			LoginFileUtility.salva(i);
 			return true;
@@ -80,5 +106,21 @@ public class LoginUtility implements Serializable{
 		return false;
 		}
 	}
-
+	
+	/**
+	 * Metodo per la rimozione di un utente dalla lista utenti
+	 * @param username Utente da rimuovere
+	 * @return Esito della rimozione	
+	 */
+	public boolean deleteUser(String username) {
+		try {
+			if(i.remove(username).equals(""))
+				return false;
+			else 
+				return true;
+		}
+		catch (NullPointerException e) {
+			return false;
+		}
+	}
 }
